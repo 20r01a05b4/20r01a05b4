13047418199a9bd885682bd20238e982ca130621
20r01a05b4 <121844180+20r01a05b4@users.noreply.github.com>
2023-04-02 15:56:39 +0530

    Delete src/check directory

--- src/check/jsxw.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-var Work=()=>{
-    return React.createElement('div',null,React.createElement('h1',{
-        id:"jsxwork"
-    },'hello jsx '));
-}
-export default Work;
