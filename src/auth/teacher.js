7dbe375c6a313952125479f39088713e51cc5064
20r01a05b4 <121844180+20r01a05b4@users.noreply.github.com>
2023-04-12 18:50:51 +0530

    Update teacher.js

--- src/auth/teacher.js
+++ src/auth/teacher.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "@firebase/app";
 const Teacher = {
-  apiKey: "AIzaSyBApHSxy8_PlxoFLHt1H5sNuBBqT9-UsRE",
-  authDomain: "faculty-44a8c.firebaseapp.com",
-  databaseURL: "https://faculty-44a8c-default-rtdb.firebaseio.com",
-  projectId: "faculty-44a8c",
-  storageBucket: "faculty-44a8c.appspot.com",
-  messagingSenderId: "181235814652",
-  appId: "1:181235814652:web:23f07d9f3850b938ae8779"
+  apiKey: "************",
+  authDomain: "*********",
+  databaseURL: "******",
+  projectId: "***************",
+  storageBucket: "*************",
+  messagingSenderId: "*************",
+  appId: "*************"
 };
 const app=initializeApp(Teacher);
 export default app
